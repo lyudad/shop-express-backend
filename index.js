af8cf4fcb9d997f914f37462fe2e158edb4ffa8b
mac <mac@email.com>
2020-08-26 19:34:59 +0300

    * process.env

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const bodyParser = require('body-parser');
 const app = express()
-const port = 8000
+const port = process.env.PORT
 
 
 app.use(bodyParser.urlencoded({ extended: true }));
